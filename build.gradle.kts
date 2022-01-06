6c4e12a09d23fb2d2f79d15e2e511fba136f023a
Krzysztof Romanowski <romanowski.kr@gmail.com>
2022-01-06 20:57:55 +0100

    Clean up git ignore and tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 
 dependencies {
     testImplementation("com.google.code.gson:gson:2.8.7")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("commons-io:commons-io:2.11.0")
 }
--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -9,7 +9,8 @@ import com.virtuslab.using_directives.custom.Parser;
 import com.virtuslab.using_directives.custom.utils.Source;
 import com.virtuslab.using_directives.custom.utils.ast.UsingTree;
 import json.CustomGsonInstance;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParserTest extends TestUtils {
   private final String inputsRoot = "parser_tests/inputs/";
@@ -38,26 +39,9 @@ public class ParserTest extends TestUtils {
             pathToInput, expectedAST.toString(), AST.toString()));
   }
 
-  @Test
-  public void testParser() {
-    compareAST("testcase1.txt", "ast1.json", "config1.json");
-    compareAST("testcase2.txt", "ast2.json", "config2.json");
-    compareAST("testcase3.txt", "ast3.json", "config3.json");
-    compareAST("testcase4.txt", "ast4.json", "config4.json");
-    compareAST("testcase5.txt", "ast5.json", "config5.json");
-    compareAST("testcase6.txt", "ast6.json", "config6.json");
-    compareAST("testcase7.txt", "ast7.json", "config7.json");
-    compareAST("testcase8.txt", "ast8.json", "config8.json");
-    compareAST("testcase9.txt", "ast9.json", "config9.json");
-    compareAST("testcase10.txt", "ast10.json", "config10.json");
-    compareAST("testcase11.txt", "ast11.json", "config11.json");
-    compareAST("testcase12.txt", "ast12.json", "config12.json");
-    compareAST("testcase13.txt", "ast13.json", "config13.json");
-    compareAST("testcase14.txt", "ast14.json", "config14.json");
-    compareAST("testcase15.txt", "ast15.json", "config15.json");
-    compareAST("testcase16.txt", "ast16.json", "config16.json");
-    compareAST("testcase17.txt", "ast17.json", "config17.json");
-    compareAST("testcase18.txt", "ast18.json", "config18.json");
-    compareAST("testcase19.txt", "ast19.json", "config19.json");
+  @ParameterizedTest(name = "Run parser testcase no. {0}")
+  @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19})
+  public void testParser(int no) {
+    compareAST("testcase" + no + ".txt", "ast" + no + ".json", "config" + no + ".json");
   }
 }
